38e80e3e88bf57f8453a7cf9153412eba35a18cb
sourav20212021 <82802288+sourav20212021@users.noreply.github.com>
2022-12-20 20:11:39 +0530

    Update Bonds_Ctrl.java
    commit-2

--- src/main/java/com/controller/Bonds_Ctrl.java
+++ src/main/java/com/controller/Bonds_Ctrl.java
@@ -1,5 +1,12 @@
 package com.controller;
 
 public class Bonds_Ctrl {
+  
+  public void bond_1(){
+    System.out.println("Bonds-1");
+  }
+  public void bond_2(){
+    System.out.println("Bonds-2");
+  }
 
 }
