d4aa395e45bc3e37598f400a4dad2fb4ce982519
sourav <souravsengupta20192019@gmail.com>
2022-12-20 23:37:55 +0530

    commit-23

--- src/main/java/com/controller/Stock_Cntrl.java
+++ src/main/java/com/controller/Stock_Cntrl.java
@@ -5,5 +5,11 @@ public class Stock_Cntrl {
 	public void cntrl_1() {
 		System.out.println("Stock Controller-1");
 	}
+	public void cntrl_2() {
+		System.out.println("Stock Controller-2");
+	}
+	public void cntrl_3() {
+		System.out.println("Stock Controller-3");
+	}
 
 }
