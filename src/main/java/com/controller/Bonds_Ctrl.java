4ddcc30d52a5d5868d00119b3ea02fe2b13afe8c
sourav <souravsengupta20192019@gmail.com>
2022-12-20 23:03:16 +0530

    commit-16

--- src/main/java/com/controller/Bonds_Ctrl.java
+++ src/main/java/com/controller/Bonds_Ctrl.java
@@ -5,26 +5,6 @@ public class Bonds_Ctrl {
   public void bond_1(){
     System.out.println("Bonds-1");
   }
-  public void bond_2(){
-    System.out.println("Bonds-2");
-  }
-  public void bond_x2(){
-    System.out.println("Bonds-2");
-  }
-  public void bond_x7(){
-	    System.out.println("Bonds-7");
-  }
-  public void bond_x0(){
-	    System.out.println("Bonds-0");
-  }
-  public void bond_x3(){
-    System.out.println("Bonds-2");
-  }	
-  public void bond_3(){
-    System.out.println("Bonds-3");
-  }
-  public void bond_4(){
-	    System.out.println("Bonds-4");
-  }
+
 
 }
